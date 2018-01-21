7bf1ce6a01b42f53bfe6c50247687dfcd24d5372
Duval Quentin <senmenty@gmail.com>
2018-01-21 11:35:00 +0100

    stack data

--- StackedBars.elm
+++ StackedBars.elm
@@ -19,40 +19,37 @@ type alias Year =
 
 
 type alias CrimeRate =
-    { year : Int
+    { year : Year
     , population : Int
     , murder : Int
     , rape : Int
     , robbery : Int
     , assault : Int
-    , burglary : Int
-    , larceny : Int
-    , motorTheft : Int
     }
 
 
 crimeRates : List CrimeRate
 crimeRates =
-    [ CrimeRate 1994 260327021 23326 102216 618949 1113179 2712774 7879812 1539287
-    , CrimeRate 1995 262803276 21606 97470 580509 1099207 2593784 7997710 1472441
-    , CrimeRate 1996 265228572 19645 96252 535594 1037049 2506400 7904685 1394238
-    , CrimeRate 1997 267783607 18208 96153 498534 1023201 2460526 7743760 1354189
-    , CrimeRate 1998 270248003 16974 93144 447186 976583 2332735 7376311 1242781
-    , CrimeRate 1999 272690813 15522 89411 409371 911740 2100739 6955520 1152075
-    , CrimeRate 2000 281421906 15586 90178 408016 911706 2050992 6971590 1160002
-    , CrimeRate 2001 285317559 16037 90863 423557 909023 2116531 7092267 1228391
-    , CrimeRate 2002 287973924 16229 95235 420806 891407 2151252 7057379 1246646
-    , CrimeRate 2003 290788976 16528 93883 414235 859030 2154834 7026802 1261226
-    , CrimeRate 2004 293656842 16148 95089 401470 847381 2144446 6937089 1237851
-    , CrimeRate 2005 296507061 16740 94347 417438 862220 2155448 6783447 1235859
-    , CrimeRate 2006 299398484 17309 94472 449246 874096 2194993 6626363 1198245
-    , CrimeRate 2007 301621157 17128 92160 447324 866358 2190198 6591542 1100472
-    , CrimeRate 2008 304059724 16465 90750 443563 843683 2228887 6586206 959059
-    , CrimeRate 2009 307006550 15399 89241 408742 812514 2203313 6338095 795652
-    , CrimeRate 2010 309330219 14722 85593 369089 781844 2168459 6204601 739565
-    , CrimeRate 2011 311587816 14661 84175 354746 752423 2185140 6151095 716508
-    , CrimeRate 2012 313873685 14856 85141 355051 762009 2109932 6168874 723186
-    , CrimeRate 2013 316128839 14196 79770 345031 724149 1928465 6004453 699594
+    [ CrimeRate 1994 260327021 23326 102216 618949 1113179
+    , CrimeRate 1995 262803276 21606 97470 580509 1099207
+    , CrimeRate 1996 265228572 19645 96252 535594 1037049
+    , CrimeRate 1997 267783607 18208 96153 498534 1023201
+    , CrimeRate 1998 270248003 16974 93144 447186 976583
+    , CrimeRate 1999 272690813 15522 89411 409371 911740
+    , CrimeRate 2000 281421906 15586 90178 408016 911706
+    , CrimeRate 2001 285317559 16037 90863 423557 909023
+    , CrimeRate 2002 287973924 16229 95235 420806 891407
+    , CrimeRate 2003 290788976 16528 93883 414235 859030
+    , CrimeRate 2004 293656842 16148 95089 401470 847381
+    , CrimeRate 2005 296507061 16740 94347 417438 862220
+    , CrimeRate 2006 299398484 17309 94472 449246 874096
+    , CrimeRate 2007 301621157 17128 92160 447324 866358
+    , CrimeRate 2008 304059724 16465 90750 443563 843683
+    , CrimeRate 2009 307006550 15399 89241 408742 812514
+    , CrimeRate 2010 309330219 14722 85593 369089 781844
+    , CrimeRate 2011 311587816 14661 84175 354746 752423
+    , CrimeRate 2012 313873685 14856 85141 355051 762009
+    , CrimeRate 2013 316128839 14196 79770 345031 724149
     ]
 
 
@@ -62,8 +59,8 @@ crimeRates =
 --------------------------------------------------------------------------------
 
 
-config : StackConfig String
-config =
+stackData : StackConfig String
+stackData =
     { data = samples series
     , offset = Shape.stackOffsetNone
 
@@ -72,17 +69,17 @@ config =
     }
 
 
-samples : Series -> List ( String, List Float )
+samples : Series CrimeRate -> List ( String, List Float )
 samples =
     List.map
         (\{ label, accessor } -> ( label, List.map (toFloat << accessor) crimeRates ))
 
 
-type alias Series =
-    List { label : String, accessor : CrimeRate -> Int }
+type alias Series input =
+    List { label : String, accessor : input -> Int }
 
 
-series : Series
+series : Series CrimeRate
 series =
     [ { label = "Assault", accessor = .assault }
     , { label = "Rape", accessor = .rape }
@@ -212,7 +209,7 @@ view canvas { values, labels, extent } =
 
 renderSvg : GraphArea -> Svg msg
 renderSvg area =
-    view area (Shape.stack config)
+    view area (Shape.stack stackData)
 
 
 translate : ( number, number ) -> Svg.Attribute msg
